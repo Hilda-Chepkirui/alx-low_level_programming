6e93329f625ef2551b0b91f03a169e8a8c4b4ae9
Hilda-Chepkirui <baraiywohilda@gmail.com>
2023-03-17 12:48:04 -0700

    Possible different combinations of two digits.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - serves as the starting point for program execution.
+ *
+ * Return: ends the execution of a function, always 0.
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 1 ; j < 10 ; j++)
+		{
+			if (i < j && i != j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i + j != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
