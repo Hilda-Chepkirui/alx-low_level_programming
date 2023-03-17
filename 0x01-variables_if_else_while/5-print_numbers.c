78988ea43e0b7bd343a38e890c7f9e5187c8e4f8
Hilda-Chepkirui <baraiywohilda@gmail.com>
2023-03-17 08:39:19 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - serves as the starting point for program execution
+ *
+ * Return: Return: ends the execution of a function, always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
