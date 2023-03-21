7f02c3c6698a7f5d536059244fa3facebbae6bc4
Hilda-Chepkirui <baraiywohilda@gmail.com>
2023-03-21 08:57:11 -0700

    prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character.
+ * @c: The character to print.
+ * Return: Returns 1 if c is a letter, lowercase or uppercase
+ * otherwis 0.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
